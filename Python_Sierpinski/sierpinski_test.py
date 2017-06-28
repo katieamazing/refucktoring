400eea77adc7c7502bccca8a9cb938d1c47e8287
katieamazing <k.allen@live.com>
2017-06-27 20:01:13 -0400

    Update sierpinski_test.py

--- Python_Sierpinski/sierpinski_test.py
+++ Python_Sierpinski/sierpinski_test.py
@@ -13,12 +13,14 @@ class SierpinskiOutputTests(unittest.TestCase):
         self.assertEqual(sierpinski(2), ['   *   ', '  * *  ', ' *   * ', '* * * *'])
 
     def test_5(self):
+        stars = 0
         for c in '\n'.join(sierpinski(5)):
             if c == '*':
                 stars += 1
         self.assertEqual(stars, 243)
 
     def test_12(self):  ## feel free to comment this one out if your computer is slower
+        stars = 0
         for c in '\n'.join(sierpinski(12)):
             if c == '*':
                 stars += 1
